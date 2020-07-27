fad5793fb2761ad1bc19442d5be5bf082f8826da
bran.nie <pengcheng1995@gmail.com>
2020-07-28 00:03:06 +0800

    1-2: 添加component

--- ToyReact.js
+++ ToyReact.js
@@ -1,7 +1,45 @@
+class ElementWrapper {
+    constructor(type) {
+        this.root = document.createElement(type);
+    }
+    setAttribute(name, value) {
+        this.root.setAttribute(name, value);
+    }
+    appendChild(vchild) {
+        vchild.mountTo(this.root);
+    }
+    mountTo(parent) {
+        parent.appendChild(this.root);
+    }
+}
+
+class TextWrapper {
+    constructor(content) {
+        this.root = document.createTextNode(content);
+    }
+    mountTo(parent) {
+        parent.appendChild(this.root);
+    }
+}
+
+export class Component {
+    setAttribute(name, value) {
+        this[name] = value;
+    }
+    mountTo(parent) {
+        let vdom = this.render();
+        vdom.mountTo(parent);
+    }
+}
+
 export const ToyReact = {
     createElement(type, attr, ...childrens) {
-        console.log(arguments);
-        let ele = document.createElement(type);
+        let ele;
+        if (typeof type === 'string') {
+            ele = new ElementWrapper(type);
+        } else {
+            ele = new type();
+        }
 
         for (let name in attr) {
             ele.setAttribute(name, attr[name]);
@@ -9,10 +47,14 @@ export const ToyReact = {
 
         for (let child of childrens) {
             if (typeof child === 'string') {
-                child = document.createTextNode(child);
+                child = new TextWrapper(child);
             }
             ele.appendChild(child);
         }
         return ele;
+    },
+    render(vdom, ele) {
+        vdom.mountTo(ele);
+        // ele.appendChild(vdom);
     },
 };
--- main.js
+++ main.js
@@ -1,16 +1,25 @@
-import { ToyReact } from './ToyReact';
+import { ToyReact, Component } from './ToyReact';
 
-// class MyComponent {}
-// console.log('test');
+class MyComponent extends Component {
+    render() {
+        return (
+            <div>
+                <span>Hello</span>
+                <span> World!</span>
+            </div>
+        );
+    }
+}
 
-// let a = <MyComponent name="a" />;
+let a = <MyComponent name="a" id="npc" />;
+ToyReact.render(a, document.body);
 
-let b = (
-    <div name="b" id="test">
-        <span>1</span>
-        <span>2</span>
-        <span>3</span>
-    </div>
-);
-document.body.appendChild(b);
-console.log(b);
+// let b = (
+//     <div name="b" id="test">
+//         <span>1</span>
+//         <span>2</span>
+//         <span>3</span>
+//     </div>
+// );
+// document.body.appendChild(b);
+// console.log(b);
